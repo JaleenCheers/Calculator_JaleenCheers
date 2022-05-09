a3e6d244753b482f18de2516376891de889b5885
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-09 18:48:05 -0400

    Adjusted size of calculator

--- Calculator_JaleenCheers/Main.cpp
+++ Calculator_JaleenCheers/Main.cpp
@@ -2,7 +2,7 @@
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
 wxEND_EVENT_TABLE()
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(400, 200)) {
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(350, 475)) {
 
 }
 Main::~Main(){
