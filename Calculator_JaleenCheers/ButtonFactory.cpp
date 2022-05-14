043e954d311132c00b1d27129750a075a30df659
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-13 21:06:17 -0400

    CreateButton methods definition and signatures added

--- Calculator_JaleenCheers/ButtonFactory.cpp
+++ Calculator_JaleenCheers/ButtonFactory.cpp
@@ -1,1 +1,126 @@
 #include "ButtonFactory.h"
+
+wxButton ButtonFactory::CreateCButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateNegativeButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateDecButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateBinButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateHexButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateMadeButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateByButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateJTCButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateSqrtButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateModButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateDivideButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateMultiplyButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateMinusButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreatePlusButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateEqualsButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateZeroButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateOneButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateTwoButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateThreeButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateFourButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateFiveButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateSixButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateSevenButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateEightButton()
+{
+	return wxButton();
+}
+
+wxButton ButtonFactory::CreateNineButton()
+{
+	return wxButton();
+}
--- Calculator_JaleenCheers/ButtonFactory.h
+++ Calculator_JaleenCheers/ButtonFactory.h
@@ -2,6 +2,37 @@
 #include "wx/wx.h"
 class ButtonFactory
 {
-	
-};
+	// Clear button
+	wxButton CreateCButton();
+	// Toggle negative button
+	wxButton CreateNegativeButton();
+	// Output mode buttons
+	wxButton CreateDecButton();	
+	wxButton CreateBinButton();			
+	wxButton CreateHexButton();			
+	// Aesthetic buttons
+	wxButton CreateMadeButton();		
+	wxButton CreateByButton();			
+	wxButton CreateJTCButton();
+	// Operator buttons
+	wxButton CreateSqrtButton();
+	wxButton CreateModButton();         
+	wxButton CreateDivideButton();
+	wxButton CreateMultiplyButton();
+	wxButton CreateMinusButton();
+	wxButton CreatePlusButton();
+	wxButton CreateEqualsButton();
+	// Number buttons
+	wxButton CreateZeroButton();		
+	wxButton CreateOneButton();
+	wxButton CreateTwoButton();			
+	wxButton CreateThreeButton();		
+	wxButton CreateFourButton();
+	wxButton CreateFiveButton();		
+	wxButton CreateSixButton();			
+	wxButton CreateSevenButton();
+	wxButton CreateEightButton();		
+	wxButton CreateNineButton();		
 
+
+};										
