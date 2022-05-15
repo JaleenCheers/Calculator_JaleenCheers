092d4fc3f48f2330d563fd7b88e69408e44af7f4
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-14 22:08:17 -0400

    Removed Stale code

--- Calculator_JaleenCheers/Main.cpp
+++ Calculator_JaleenCheers/Main.cpp
@@ -1,5 +1,5 @@
 #include "Main.h"
-wxBEGIN_EVENT_TABLE(Main, wxFrame)      // Maybe make this a for loop
+wxBEGIN_EVENT_TABLE(Main, wxFrame)     
 EVT_BUTTON(10000, Main::OnButtonClicked)   // C
 //EVT_BUTTON(10001, Main::OnButtonClicked) // Dec
 //EVT_BUTTON(10002, Main::OnButtonClicked) // Bin
@@ -10,7 +10,7 @@ EVT_BUTTON(10006, Main::OnButtonClicked)   // 9
 EVT_BUTTON(10007, Main::OnButtonClicked)   // 6
 EVT_BUTTON(10008, Main::OnButtonClicked)   // 3
 //EVT_BUTTON(10009, Main::OnButtonClicked) // Made 
-EVT_BUTTON(10010, Main::OnButtonClicked) // +/-
+EVT_BUTTON(10010, Main::OnButtonClicked)   // +/-
 EVT_BUTTON(10011, Main::OnButtonClicked)   // 8
 EVT_BUTTON(10012, Main::OnButtonClicked)   // 5
 EVT_BUTTON(10013, Main::OnButtonClicked)   // 2
@@ -27,12 +27,11 @@ EVT_BUTTON(10023, Main::OnButtonClicked)   // +
 //EVT_BUTTON(10024, Main::OnButtonClicked) // =
 wxEND_EVENT_TABLE()
 
-// wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX
 
 Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(475, 500), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)) {
 
+	// Variables
 	ButtonFactory fact;
-	//btn = new wxButton * [mFieldHeight * mFieldWidth];
 	wxGridSizer* grid = new wxGridSizer(mFieldWidth, mFieldHeight, -100, 25);
 	text = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 0), wxSize(475, 100), wxCB_READONLY);
 
@@ -40,8 +39,6 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(
 	SetBackgroundColour("#577590");
 	wxFont font(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_SLANT, wxFONTWEIGHT_BOLD, false);
 	SetFont(font);
-
-	int num = 9;
 
 
 	// Make All the buttons
@@ -72,7 +69,7 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(
 	btnEquals = fact.CreateButton(this, 10024, "=", wxDefaultPosition, wxSize(75, 60));
 
 
-
+	// Add all buttons to the grid
 	grid->Add(btnClear, 1, wxALIGN_BOTTOM);
 	grid->Add(btnSqrt, 1, wxALIGN_BOTTOM);
 	grid->Add(btnNegative, 1, wxALIGN_BOTTOM);
@@ -99,98 +96,7 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSize(
 	grid->Add(btnJTC, 1, wxALIGN_BOTTOM);
 	grid->Add(btnEquals, 1, wxALIGN_BOTTOM);
 
-	/*for (int i = 0; i < 25; ++i) {
-
-		switch (i)
-		{
-		case 1:
-			btn[i] = fact.CreateCButton();
-			break;
-		default:
-			break;
-		}
-
-		grid->Add(btn[i]);
-
-	}*/
-
-
-	//grid->Add(ButtonFactory::CreateCButton());
-
-	//for (int x = 0; x < mFieldWidth; ++x) {
-	//	for (int y = 0; y < mFieldHeight; ++y) {
-
-
-	//		// Fill window with button using a grid and add color
-	//		btn[y * mFieldWidth + x] = new wxButton(this, 10000 + (y * mFieldWidth + x), "", wxDefaultPosition, wxSize(75, 60));
-	//		grid->Add(btn[y * mFieldWidth + x], 1, wxALIGN_BOTTOM);
-	//		btn[y * mFieldWidth + x]->SetBackgroundColour(wxColor("#adb5bd"));
-
-	//		// Set button labels 
-	//		switch (counter)
-	//		{
-	//		case 0:
-	//			btn[y * mFieldWidth + x]->SetLabel("C");
-	//			break;
-	//		case 1:
-	//			btn[y * mFieldWidth + x]->SetLabel("Sqrt");
-	//			break;
-	//		case 2:
-	//			btn[y * mFieldWidth + x]->SetLabel("+/-");
-	//			break;
-	//		case 3:
-	//			btn[y * mFieldWidth + x]->SetLabel("Mod");
-	//			break;
-	//		case 4:
-	//			btn[y * mFieldWidth + x]->SetLabel('/');
-	//			break;
-	//		case 5:
-	//			btn[y * mFieldWidth + x]->SetLabel("Dec.");
-	//			break;
-	//		case 9:
-	//			btn[y * mFieldWidth + x]->SetLabel('x');
-	//			break;
-	//		case 10:
-	//			btn[y * mFieldWidth + x]->SetLabel("Bin.");
-	//			break;
-	//		case 14:
-	//			btn[y * mFieldWidth + x]->SetLabel('-');
-	//			break;
-	//		case 15:
-	//			btn[y * mFieldWidth + x]->SetLabel("Hex.");
-	//			break;
-	//		case 19:
-	//			btn[y * mFieldWidth + x]->SetLabel('+');
-	//			break;
-	//		case 21:
-	//			btn[y * mFieldWidth + x]->SetLabel("Made");
-	//			break;
-	//		case 22:
-	//			btn[y * mFieldWidth + x]->SetLabel("By");
-	//			break;
-	//		case 23:
-	//			btn[y * mFieldWidth + x]->SetLabel("JTC");
-	//			break;
-	//		case 24:
-	//			btn[y * mFieldWidth + x]->SetLabel('=');
-	//			break;
-	//		default:
-	//			btn[y * mFieldWidth + x]->SetLabel(std::to_string(num--));
-	//			break;
-	//		}
-
-
-
-	//		//Increment counter for switch case
-	//		++counter;
-
-
-
-
-
-
-	//	}
-	//}
+	
 
 	this->SetSizer(grid);
 	grid->Layout();
