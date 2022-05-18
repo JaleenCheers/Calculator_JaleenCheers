d110bdc960b3d0d2c36f55da8727ec7faf958074
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-18 17:18:47 -0400

    j

--- Calculator_JaleenCheers/CalculatorProcessor.cpp
+++ Calculator_JaleenCheers/CalculatorProcessor.cpp
@@ -1,1 +1,8 @@
 #include "CalculatorProcessor.h"
+
+CalculatorProcessor* CalculatorProcessor::GetInstance()
+{
+	if (calcProcessor == nullptr)
+		calcProcessor = new CalculatorProcessor();
+	return calcProcessor;
+}
--- Calculator_JaleenCheers/CalculatorProcessor.h
+++ Calculator_JaleenCheers/CalculatorProcessor.h
@@ -1,8 +1,13 @@
 #pragma once
-static class CalculatorProcessor
+#include <wx/wx.h>
+ class CalculatorProcessor
 {
 private:
-
+	static CalculatorProcessor* calcProcessor;
+	CalculatorProcessor() {
+		calcProcessor = nullptr;
+	}
 public:
+	static CalculatorProcessor* GetInstance();
 };
 
