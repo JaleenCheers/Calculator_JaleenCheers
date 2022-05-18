a4c1afba1c87328cf2a0baa83c3d0b15fd4d910b
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-18 17:34:07 -0400

    GetInstance implemented

--- Calculator_JaleenCheers/CalculatorProcessor.h
+++ Calculator_JaleenCheers/CalculatorProcessor.h
@@ -8,7 +8,12 @@ public:
 	CalculatorProcessor(CalculatorProcessor& c) = delete;
 	void operator= (const CalculatorProcessor& c) = delete;
 
+	static CalculatorProcessor* GetInstance() {
+		if (mCalcProcessor == nullptr)
+			mCalcProcessor = new CalculatorProcessor();
 
+		return mCalcProcessor;
+	}
 	
 	
 };
