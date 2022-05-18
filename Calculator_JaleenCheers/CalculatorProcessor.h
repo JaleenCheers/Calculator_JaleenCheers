f454e0a409bd867d1711cebb161c49d1821ab421
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-18 17:23:38 -0400

    Copy constructor and assignment operator = delete;

--- Calculator_JaleenCheers/CalculatorProcessor.h
+++ Calculator_JaleenCheers/CalculatorProcessor.h
@@ -1,6 +1,9 @@
 #pragma once
 class CalculatorProcessor
 {
-
+private:
+public:
+	CalculatorProcessor(CalculatorProcessor& c) = delete;
+	void operator= (const CalculatorProcessor& c) = delete;
 };
 
