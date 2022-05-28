5c65e785d37eb1817d5aa0ad2a44147cbbf6c36e
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-27 20:57:34 -0400

    Test method signature added in for ButtonFactoryTests

---
+++ CalculatorTests/ButtonFactoryTests.cpp
@@ -0,0 +1,41 @@
+#include "ButtonFactoryTests.h"
+
+void ButtonFactoryTests::TestButtonLabel1()
+{
+}
+
+void ButtonFactoryTests::TestButtonLabel2()
+{
+}
+
+void ButtonFactoryTests::TestButtonLabel3()
+{
+}
+
+void ButtonFactoryTests::TestButtonLabel4()
+{
+}
+
+void ButtonFactoryTests::TestButtonId1()
+{
+}
+
+void ButtonFactoryTests::TestButtonId2()
+{
+}
+
+void ButtonFactoryTests::TestButtonId3()
+{
+}
+
+void ButtonFactoryTests::TestButtonId4()
+{
+}
+
+void ButtonFactoryTests::TestButtonValue1()
+{
+}
+
+void ButtonFactoryTests::TestButtonValue2()
+{
+}
---
+++ CalculatorTests/ButtonFactoryTests.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../Calculator_JaleenCheers/ButtonFactory.h"
+#include "wx/wx.h"
+class ButtonFactoryTests
+{
+	void TestButtonLabel1();
+	void TestButtonLabel2();
+	void TestButtonLabel3();
+	void TestButtonLabel4();
+	void TestButtonId1();
+	void TestButtonId2();
+	void TestButtonId3();
+	void TestButtonId4();
+	void TestButtonValue1();
+	void TestButtonValue2();
+
+
+};
+
