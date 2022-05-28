60c9460b929f387317b72b825a0a3371d65fa85f
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-27 21:13:27 -0400

    TestButtonId methods finished

--- CalculatorTests/ButtonFactoryTests.cpp
+++ CalculatorTests/ButtonFactoryTests.cpp
@@ -44,18 +44,42 @@ void ButtonFactoryTests::TestButtonLabel4()
 
 void ButtonFactoryTests::TestButtonId1()
 {
+	btn = buttonFactory.CreateButton(new wxWindow(), 10018, "1", wxDefaultPosition, wxSize(75, 60));
+
+	if (btn->GetId() == 10018)
+		std::cout << "PASS" << std::endl;
+	else
+		std::cout << "FAIL" << std::endl;
 }
 
 void ButtonFactoryTests::TestButtonId2()
 {
+	btn = buttonFactory.CreateButton(new wxWindow(), 10013, "2", wxDefaultPosition, wxSize(75, 60));
+
+	if (btn->GetId() == 10013)
+		std::cout << "PASS" << std::endl;
+	else
+		std::cout << "FAIL" << std::endl;
 }
 
 void ButtonFactoryTests::TestButtonId3()
 {
+	btn = buttonFactory.CreateButton(new wxWindow(), 10008, "3", wxDefaultPosition, wxSize(75, 60));
+
+	if (btn->GetId() == 10008)
+		std::cout << "PASS" << std::endl;
+	else
+		std::cout << "FAIL" << std::endl;
 }
 
 void ButtonFactoryTests::TestButtonId4()
 {
+	btn = buttonFactory.CreateButton(new wxWindow(), 10017, "4", wxDefaultPosition, wxSize(75, 60));
+
+	if (btn->GetId() == 10017)
+		std::cout << "PASS" << std::endl;
+	else
+		std::cout << "FAIL" << std::endl;
 }
 
 void ButtonFactoryTests::TestButtonValue1()
