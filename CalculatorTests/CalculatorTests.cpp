13aaba0d2f021d08ef68355e2fbc6859c993d1af
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-27 21:20:16 -0400

    ButtonF

--- CalculatorTests/CalculatorTests.cpp
+++ CalculatorTests/CalculatorTests.cpp
@@ -2,10 +2,22 @@
 //
 
 #include <iostream>
+#include "ButtonFactoryTests.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  //  ButtonFactoryTests buttonFactoryTests;
+
+   /* buttonFactoryTests.TestButtonId1();
+    buttonFactoryTests.TestButtonId2();
+    buttonFactoryTests.TestButtonId3();
+    buttonFactoryTests.TestButtonId4();
+    buttonFactoryTests.TestButtonLabel1();
+    buttonFactoryTests.TestButtonLabel2();
+    buttonFactoryTests.TestButtonLabel3();
+    buttonFactoryTests.TestButtonLabel4();
+    buttonFactoryTests.TestButtonValue1();
+    buttonFactoryTests.TestButtonValue2();*/
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
